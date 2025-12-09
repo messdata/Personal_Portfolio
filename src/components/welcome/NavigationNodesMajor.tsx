28ed46830bd9da9d3131fa12c6b5a331fa9d6ca3
Chinmay Patil <147683529+messdata@users.noreply.github.com>
2025-12-09 16:19:09 +0000

    Update import for NavigationNode component

--- src/components/welcome/NavigationNodesMajor.tsx
+++ src/components/welcome/NavigationNodesMajor.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import NavigationNode from './Navigationnode';
+import NavigationNode from './NavigationNodeMini.tsx';
 
 interface NavigationNodesProps {
     isVisible?: boolean;
